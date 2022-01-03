a9b4d540ef91ea231b4f90ff2291da9df630db6a
tatsuya4559 <tatsuya.k1029@gmail.com>
2022-01-03 16:59:25 +0900

    delete default components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,6 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
   }
 ];
 @NgModule({
