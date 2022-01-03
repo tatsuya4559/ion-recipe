d4805ee8d162d7a031e8cd1e4cad8725ace0f3c7
tatsuya4559 <tatsuya.k1029@gmail.com>
2022-01-03 17:13:58 +0900

    fill recipe items

--- src/app/recipes/recipes.page.ts
+++ src/app/recipes/recipes.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipe.model';
 
 @Component({
   selector: 'app-recipes',
@@ -6,6 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes.page.scss'],
 })
 export class RecipesPage implements OnInit {
+  recipes: Recipe[] = [
+    {
+      id: 'r1',
+      title: 'Test',
+      imageUrl: 'https://bst-image.imgix.net/prod-gixo/content/uploads/2020/12/test.png?auto=compress%2Cformat&ixlib=php-1.2.1',
+      ingredients: ['Apple', 'Banana', 'Potato'],
+    },
+    {
+      id: 'r2',
+      title: 'Test2',
+      imageUrl: 'https://bst-image.imgix.net/prod-gixo/content/uploads/2020/12/test.png?auto=compress%2Cformat&ixlib=php-1.2.1',
+      ingredients: ['Meat'],
+    },
+  ];
 
   constructor() { }
 
