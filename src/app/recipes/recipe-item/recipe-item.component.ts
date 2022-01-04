adaffe75bcd5da7c58df536d34fe8c7fdcb4a132
tatsuya4559 <tatsuya.k1029@gmail.com>
2022-01-05 07:54:06 +0900

    add delete slide button

--- src/app/recipes/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-item/recipe-item.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipesService } from '../recipes.service';
 
 @Component({
   selector: 'app-recipe-item',
@@ -9,8 +10,12 @@ import { Recipe } from '../recipe.model';
 export class RecipeItemComponent implements OnInit {
   @Input() recipe: Recipe;
 
-  constructor() { }
+  constructor(private recipesService: RecipesService) { }
 
   ngOnInit() {}
+
+  onDelete() {
+    this.recipesService.deleteRecipe(this.recipe.id);
+  }
 
 }
