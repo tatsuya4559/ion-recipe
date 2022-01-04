b043cdffa0db4bb80a81d525fb8680e6b5aa49dc
tatsuya4559 <tatsuya.k1029@gmail.com>
2022-01-04 20:30:20 +0900

    retrieve recipes from service

--- src/app/recipes/recipes.page.ts
+++ src/app/recipes/recipes.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { RecipesService } from './recipes.service';
 
 @Component({
   selector: 'app-recipes',
@@ -7,24 +8,12 @@ import { Recipe } from './recipe.model';
   styleUrls: ['./recipes.page.scss'],
 })
 export class RecipesPage implements OnInit {
-  recipes: Recipe[] = [
-    {
-      id: 'r1',
-      title: 'Test',
-      imageUrl: 'https://bst-image.imgix.net/prod-gixo/content/uploads/2020/12/test.png?auto=compress%2Cformat&ixlib=php-1.2.1',
-      ingredients: ['Apple', 'Banana', 'Potato'],
-    },
-    {
-      id: 'r2',
-      title: 'Test2',
-      imageUrl: 'https://bst-image.imgix.net/prod-gixo/content/uploads/2020/12/test.png?auto=compress%2Cformat&ixlib=php-1.2.1',
-      ingredients: ['Meat'],
-    },
-  ];
+  recipes: Recipe[];
 
-  constructor() { }
+  constructor(private recipesService: RecipesService) { }
 
   ngOnInit() {
+    this.recipes = this.recipesService.getAllRecipes();
   }
 
 }
--- src/app/recipes/recipes.service.ts
+++ src/app/recipes/recipes.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { Recipe } from './recipe.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecipesService {
+  private recipes: Recipe[] = [
+    {
+      id: 'r1',
+      title: 'Test',
+      imageUrl: 'https://bst-image.imgix.net/prod-gixo/content/uploads/2020/12/test.png?auto=compress%2Cformat&ixlib=php-1.2.1',
+      ingredients: ['Apple', 'Banana', 'Potato'],
+    },
+    {
+      id: 'r2',
+      title: 'Test2',
+      imageUrl: 'https://bst-image.imgix.net/prod-gixo/content/uploads/2020/12/test.png?auto=compress%2Cformat&ixlib=php-1.2.1',
+      ingredients: ['Meat'],
+    },
+  ];
 
   constructor() { }
+
+  getAllRecipes() {
+    return [...this.recipes];
+  }
+
+  getRecipe(recipeId: string) {
+    return {
+      ...this.recipes.find(recipe => recipe.id === recipeId)
+    };
+  }
 }
