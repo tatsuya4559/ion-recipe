c8ea87808c9e7a21b60933995ce90365491b158a
tatsuya4559 <tatsuya.k1029@gmail.com>
2022-01-06 20:10:16 +0900

    add pages

--- src/app/places/places-routing.module.ts
+++ src/app/places/places-routing.module.ts
@@ -7,6 +7,14 @@ const routes: Routes = [
   {
     path: '',
     component: PlacesPage
+  },
+  {
+    path: 'discover',
+    loadChildren: () => import('./discover/discover.module').then( m => m.DiscoverPageModule)
+  },
+  {
+    path: 'offers',
+    loadChildren: () => import('./offers/offers.module').then( m => m.OffersPageModule)
   }
 ];
 
