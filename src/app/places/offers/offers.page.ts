a38f271efb28f9fecf7a432f1442e9f6c55473ec
tatsuya4559 <tatsuya.k1029@gmail.com>
2022-01-08 13:19:24 +0900

    slide

--- src/app/places/offers/offers.page.ts
+++ src/app/places/offers/offers.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IonItemSliding, NavController } from '@ionic/angular';
 import { Place } from '../place.model';
 import { PlacesService } from '../places.service';
 
@@ -10,10 +11,17 @@ import { PlacesService } from '../places.service';
 export class OffersPage implements OnInit {
   offers: Place[];
 
-  constructor(private placesService: PlacesService) { }
+  constructor(
+    private placesService: PlacesService,
+    private navCtrl: NavController,
+  ) { }
 
   ngOnInit() {
     this.offers = this.placesService.places;
   }
 
+  async onEdit(offerId: string, slidingItem: IonItemSliding) {
+    await slidingItem.close();
+    this.navCtrl.navigateForward(`/places/tabs/offers/edit/${offerId}`);
+  }
 }
