778b3b54bc144700fc34d92f17b3916c8c8fa227
tatsuya4559 <tatsuya.k1029@gmail.com>
2022-01-08 14:04:40 +0900

    implement bookings

--- src/app/bookings/booking.service.ts
+++ src/app/bookings/booking.service.ts
@@ -1,9 +1,49 @@
 import { Injectable } from '@angular/core';
+import { Booking } from './booking.model';
+import { of, defer, concat, Subject } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookingService {
+  private _bookings: Booking[] = [
+    {
+      id: 'hoge',
+      placeId: 'p1',
+      placeTitle: 'Tokyo Mansion',
+      guestNumber: 100,
+      userId: 'kato',
+    },
+    {
+      id: 'fuga',
+      placeId: 'p2',
+      placeTitle: 'Osaka Mansion',
+      guestNumber: 11,
+      userId: 'sato',
+    }
+  ];
+
+  get bookings() {
+    return [...this._bookings];
+  }
+
+  set bookings(newBookings: Booking[]) {
+    this._bookings = newBookings;
+    this.bookingsChanged$.next();
+  }
+
 
   constructor() { }
+
+  bookingsChanged$ = new Subject<void>();
+
+  bookings$ = concat(
+    defer(() => of(this.bookings)), // 初回
+    this.bookingsChanged$.pipe(map(() => this.bookings)) // 更新時
+  );
+
+  cancelBooking(bookingId: string) {
+    this.bookings = this._bookings.filter(b => b.id !== bookingId);
+  }
 }
--- src/app/bookings/bookings.page.ts
+++ src/app/bookings/bookings.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { IonItemSliding } from '@ionic/angular';
+import { Booking } from './booking.model';
+import { BookingService } from './booking.service';
 
 @Component({
   selector: 'app-bookings',
@@ -6,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bookings.page.scss'],
 })
 export class BookingsPage implements OnInit {
+  loadedBookings: Booking[];
 
-  constructor() { }
+  constructor(
+    private bookingService: BookingService,
+  ) { }
 
   ngOnInit() {
+    this.bookingService.bookings$.subscribe(bookings => {
+      this.loadedBookings = bookings;
+    });
+  }
+
+  onCancelBooking(bookingId: string, slidingItem: IonItemSliding) {
+    slidingItem.close();
+    this.bookingService.cancelBooking(bookingId);
   }
 
 }
