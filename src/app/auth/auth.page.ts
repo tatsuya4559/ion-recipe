16512015148eeef0961e66a491701d23ac0582d5
tatsuya4559 <tatsuya.k1029@gmail.com>
2022-01-13 19:43:21 +0900

    laoding

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NavController } from '@ionic/angular';
+import { LoadingController, NavController } from '@ionic/angular';
 import { AuthService } from '../auth.service';
 
 @Component({
@@ -8,18 +8,35 @@ import { AuthService } from '../auth.service';
   styleUrls: ['./auth.page.scss'],
 })
 export class AuthPage implements OnInit {
+  isLoading = false;
 
   constructor(
     private authService: AuthService,
     private navCtrl: NavController,
+    private loadingController: LoadingController,
   ) { }
 
   ngOnInit() {
   }
 
-  onLogin() {
+  async onLogin() {
     this.authService.login();
+    await this.presentLoading();
     this.navCtrl.navigateRoot('/places/tabs/discover');
+
+    // this.isLoading = true;
+    // setTimeout(() => {
+    //   this.isLoading = false;
+    //   this.navCtrl.navigateRoot('/places/tabs/discover');
+    // }, 1500);
   }
 
+  async presentLoading() {
+    const loading = await this.loadingController.create({
+      message: 'Please wait...',
+      duration: 1500,
+    });
+    await loading.present();
+    await loading.onDidDismiss();
+  }
 }
